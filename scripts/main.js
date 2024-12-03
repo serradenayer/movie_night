5fba893e25b4e8b27834fb1af3b1ae6652c08f72
serradenayer <serradenayer@outlook.com>
2024-12-02 21:34:33 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
           movieContent += `
             <div>
               <h3>${movie.title}</h3>
-              <h3>${movie.time}</h3>
+              <h5>${movie.time}</h5>
               <p>${movie.description}</p>
             </div>
           `;
